bb23cb1515ff7f14293f37b973441ef8fc960761
Navd15 <gill.222navdeep@gmail.com>
2017-12-20 00:29:26 +0530

    Ui changes in editor activity

---
+++ app/src/main/java/brewcrew/com/taskmanager/Db/databaseEntries.java
@@ -0,0 +1,16 @@
+package brewcrew.com.taskmanager.Db;
+
+/**
+ * Created by navdeep on 11/12/2017.
+ */
+
+public final class databaseEntries {
+    public static final String ID="S.no";
+    public static final String tableName = "taskDB";
+    public static final String description = "desc";
+    public static final String date = "date";
+    public static final String title ="title";
+    public static final String time ="time"; 
+
+
+}
