c122f46403e94affd250ff75e9b8f3e6265df92d
Navd15 <gill.222navdeep@gmail.com>
2018-01-03 23:27:40 +0530

    helper class changes

--- app/src/main/java/brewcrew/com/taskmanager/helperClasses/MyTasks.java
+++ app/src/main/java/brewcrew/com/taskmanager/helperClasses/MyTasks.java
@@ -4,13 +4,14 @@ package brewcrew.com.taskmanager.helperClasses;
  * Created by navdeep on 09/11/2017.
  */
 
+
 public class MyTasks {
     String title;
     String desc;
     String date;
     boolean isCompleted;
     String time;
-
+    private static final String TAG = "MyTasks";
 
 
 
@@ -21,12 +22,21 @@ public class MyTasks {
         
     }
 
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
     public MyTasks(String d1, String t1, String d, String tym, boolean completed) {
 
         date=d1;
         title=t1;
 
         desc=d;
+
         isCompleted=completed;
         time=tym;
     }
