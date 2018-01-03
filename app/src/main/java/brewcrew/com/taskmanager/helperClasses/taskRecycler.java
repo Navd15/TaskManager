03dc724941750ab5f4d4128386100566defbda4b
Navd15 <gill.222navdeep@gmail.com>
2018-01-03 23:26:56 +0530

    touchlistener interface added

--- app/src/main/java/brewcrew/com/taskmanager/helperClasses/taskRecycler.java
+++ app/src/main/java/brewcrew/com/taskmanager/helperClasses/taskRecycler.java
@@ -3,8 +3,10 @@ package brewcrew.com.taskmanager.helperClasses;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -16,12 +18,23 @@ import brewcrew.com.taskmanager.R;
  * Created by navdeep on 09/11/2017.
  */
 
+
 public class taskRecycler extends RecyclerView.Adapter<taskRecycler.taskHolders> {
     private static final String TAG = "taskRecycler";
 
 
-    public class taskHolders extends RecyclerView.ViewHolder{
+    public interface touchListener {
+
+        void onCleck(int clickedIndex);
+
+    }
+
+    final private touchListener listener;
+
+    public class taskHolders extends RecyclerView.ViewHolder implements View.OnClickListener {
+
         private static final String TAG = "taskHolders";
+
         TextView date;
         TextView task_title;
         TextView task_des;
@@ -30,49 +43,59 @@ public class taskRecycler extends RecyclerView.Adapter<taskRecycler.taskHolders>
         public taskHolders(View itemView) {
             super(itemView);
             Log.i(TAG, "taskHolders: runned");
-            //Intialize views frrom recycler_task.html
-            //date field
+            //Intialize views from recycler_task.xml
 
-           date=(TextView) itemView.findViewById(R.id.date_view);
+            //date field
+            date = (TextView) itemView.findViewById(R.id.date_view);
 //            title field
-           task_title=(TextView) itemView.findViewById(R.id.title_view);
+            task_title = (TextView) itemView.findViewById(R.id.title_view);
 //           description field
-           task_des=(TextView) itemView.findViewById(R.id.des_view);
+            task_des = (TextView) itemView.findViewById(R.id.des_view);
+
+            itemView.setOnClickListener(this);
 
         }
 
 
+        @Override
+        public void onClick(View view) {
+            int position = getAdapterPosition();
+            listener.onCleck(position);
+        }
     }
 
 
-    List<MyTasks> tasks=new ArrayList<MyTasks>();
+    List<MyTasks> tasks = new ArrayList<MyTasks>();
     //constructor will intialize the List of tasks
-    public taskRecycler(List tasks) {
-        this.tasks=tasks;
+
+    public taskRecycler(List tasks, touchListener tcl) {
+        this.tasks = tasks;
+        listener = tcl;
         Log.i(TAG, "taskRecycler: runned");
     }
 
     @Override
     public taskHolders onCreateViewHolder(ViewGroup parent, int viewType) {
-       View view=LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_task,null,false);
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_task, null, false);
+
         return new taskHolders(view);
     }
 
     @Override
     public void onBindViewHolder(taskHolders holder, int position) {
-        Log.i(TAG, "onBindViewHolder:"+tasks.get(position).getDate());
+
+        Log.i(TAG, "onBindViewHolder:" + tasks.get(position).getDate());
         holder.date.setText(tasks.get(position).getDate());
         holder.task_title.setText(tasks.get(position).getTitle());
         holder.task_des.setText(tasks.get(position).getDesc());
         Log.i(TAG, "onBindViewHolder: runned");
 
-
-
     }
 
     @Override
     public int getItemCount() {
-        Log.i(TAG, "getItemCount:"+tasks.size());
+        Log.i(TAG, "getItemCount:" + tasks.size());
         return tasks.size();
     }
 
