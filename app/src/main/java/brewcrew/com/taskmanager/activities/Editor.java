2fac13bfc96eca33b3ae1d1fe48796d6efb6fb75
Navd15 <gill.222navdeep@gmail.com>
2018-01-06 20:52:53 +0530

    common bug fixes and changes reflected

--- app/src/main/java/brewcrew/com/taskmanager/activities/Editor.java
+++ app/src/main/java/brewcrew/com/taskmanager/activities/Editor.java
@@ -1,17 +1,14 @@
 package brewcrew.com.taskmanager.activities;
 
-import android.content.ComponentName;
-import android.content.pm.PackageManager;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import java.util.Calendar;
 
 import brewcrew.com.taskmanager.Pickers.datePicker;
 import brewcrew.com.taskmanager.Pickers.timePicker;
@@ -19,12 +16,10 @@ import brewcrew.com.taskmanager.R;
 import brewcrew.com.taskmanager.helperClasses.MyTasks;
 
 public class Editor extends AppCompatActivity {
-    Calendar calendar = Calendar.getInstance();
+    private static final String TAG = "Editor";
     public static TextView date, timeView;
     public static EditText desc, titl;
-    private static final String TAG = "Editor";
-    private String sender;
-   private MyTasks tasks;
+    private MyTasks tasks;
 
 
     @Override
@@ -32,22 +27,19 @@ public class Editor extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
         getSupportActionBar().setTitle(null);
-        String tareek = getIntent().getStringExtra("default date");
         date = (TextView) findViewById(R.id.date);
-        date.setText(tareek);
         titl = (EditText) findViewById(R.id.titl);
         desc = (EditText) findViewById(R.id.desc);
-        desc.setText(desc.getText());
         timeView = (TextView) findViewById(R.id.time_view);
 
-if(getIntent().hasExtra("from_onCleck")){
-    tasks= (MyTasks) MainActivity.li.get(getIntent().getIntExtra("from_onCleck",0));
-    date.setText(tasks.getDate());
-    titl.setText(tasks.getTitle());
-    desc.setText(tasks.getDesc());
-    timeView.setText(tasks.getTime());
+        if (getIntent().hasExtra("from_onCleck")) {
+            tasks = (MyTasks) MainActivity.li.get(getIntent().getIntExtra("from_onCleck", 0));
+            date.setText(tasks.getDate());
+            titl.setText(tasks.getTitle());
+            desc.setText(tasks.getDesc());
+            timeView.setText(tasks.getTime());
 
-}
+        }
 
 
     }
@@ -55,32 +47,74 @@ if(getIntent().hasExtra("from_onCleck")){
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if(tasks!=null){
-            if(tasks.getDate()==date.getText().toString()){}
+        if (keyCode == KeyEvent.KEYCODE_BACK)
+            if (tasks != null) {
+                if (!tasks.getDate().toString().equals(date.getText().toString()) |!tasks.getDesc().toString().equals(desc.getText().toString() ) | !
+                        tasks.getTitle() .equals(titl.getText().toString().toString())
+                         | !tasks.getTime().toString().equals( timeView.getText().toString()) ) {
 
-        }
-        else
-       if (keyCode == KeyEvent.KEYCODE_BACK && desc.getText().toString().length() != 0) {
+                    tasks.setDesc(desc.getText().toString());
+                    tasks.setDate(date.getText().toString());
+                    tasks.setTime(timeView.getText().toString());
+                    tasks.setTitle(titl.getText().toString());
+                    int temp_pos = getIntent().getIntExtra("from_onCleck", 0);
+//                    MainActivity.li.add(temp_pos, tasks);
+//                    MainActivity.li.remove(temp_pos);
+                    super.onKeyDown(keyCode, event);
+                }
 
-       //  new MyTasks(textView.getText().toString(),ET.getText().subSequence(0,11).toString(),ET.getText().toString());
-            MyTasks m = new MyTasks(date.getText().toString(),titl.getText().toString(),
-                    desc.getText().toString(),
-                    timeView.getText().toString()
-                    , false);
+            } else if (desc.getText().toString().length() != 0) {
+
+                if (date.getText().toString().length() != 0) {
+                    String tym = null;
+                    if (timeView.getText().toString().length() != 0) {
+                        tym = timeView.getText().toString();
+                    } else
+                        tym = "00:00";
+                    MyTasks m = new MyTasks(date.getText().toString(), titl.getText().toString(),
+                            desc.getText().toString(),
+                            tym
+                            , false);
 
 
-            MainActivity.li.add(m);
+                    MainActivity.li.add(m);
+                } else {
+
+                    AlertDialog.Builder ad = new AlertDialog.Builder(this, R.style.ThemeOverlay_AppCompat_Dialog);
+                    ad.setMessage("Please select date for the task");
+                    ad.setPositiveButton("Set date", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i) {
+                            showDatePicker();
+                        }
+                    });
+
+                    ad.setCancelable(true);
+
+                    AlertDialog adg = ad.create();
+                    adg.show();
 
 
-        }
-
+                }
+            }
 
         return super.onKeyDown(keyCode, event);
     }
 
+
     public void showDatePicker(View v) {
         datePicker pic = new datePicker();
-        pic.show(getSupportFragmentManager(), "datePicker");
+        pic.show(getSupportFragmentManager(), "datepicker");
+
+//    DialogFragment  dialogFragment=new DialogFragment();
+//    dialogFragment.show(getSupportFragmentManager(),"datePicker.class");
+
+    }
+
+    public void showDatePicker() {
+        datePicker pic = new datePicker();
+        pic.show(getSupportFragmentManager(), "datepicker");
+
 //    DialogFragment  dialogFragment=new DialogFragment();
 //    dialogFragment.show(getSupportFragmentManager(),"datePicker.class");
 
@@ -90,5 +124,11 @@ if(getIntent().hasExtra("from_onCleck")){
         new timePicker().show(getFragmentManager(), "time_picker");
 
 
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        return super.onTouchEvent(event);
     }
 }
