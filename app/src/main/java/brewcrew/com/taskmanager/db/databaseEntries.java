77edff8ae4553db08720d6eb135ba3cf83a20c2c
navdeep <gill.222navdeep@gmail.com>
2018-03-04 02:08:10 +0530

    db entries and columns added + package move

---
+++ app/src/main/java/brewcrew/com/taskmanager/db/databaseEntries.java
@@ -0,0 +1,22 @@
+package brewcrew.com.taskmanager.db;
+/**
+ * Created by navdeep on 11/12/2017.
+ */
+
+public final class databaseEntries {
+
+    public static final String ID = "S_no";
+    public static final String tableName = "taskDB";
+    public static final String description = "db_desc";
+    public static final String date = "db_date";
+    public static final String title = "db_title";
+    public static final String time = "db_time";
+    public static final String status = "db_status";
+    public static final String notifyUser="db_notifyUser";
+    public static final String selectAllQuery = "SELECT * FROM " + tableName;
+
+
+
+
+
+}
