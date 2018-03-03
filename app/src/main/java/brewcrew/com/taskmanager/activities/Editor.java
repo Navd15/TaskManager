dca913222a38c0e5e1c98994bf504d28effda952
navdeep <gill.222navdeep@gmail.com>
2018-03-04 02:05:13 +0530

    db schema changes

--- app/src/main/java/brewcrew/com/taskmanager/activities/Editor.java
+++ app/src/main/java/brewcrew/com/taskmanager/activities/Editor.java
@@ -1,119 +1,115 @@
 package brewcrew.com.taskmanager.activities;
-
 import android.app.FragmentManager;
+import android.content.ContentValues;
 import android.content.DialogInterface;
-import android.content.SharedPreferences;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.preference.Preference;
-import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Calendar;
 
-import brewcrew.com.taskmanager.Pickers.datePicker;
-import brewcrew.com.taskmanager.Pickers.timePicker;
+import brewcrew.com.taskmanager.db.database;
+import brewcrew.com.taskmanager.db.databaseEntries;
+import brewcrew.com.taskmanager.pickers.datePicker;
+import brewcrew.com.taskmanager.pickers.timePicker;
 import brewcrew.com.taskmanager.R;
-import brewcrew.com.taskmanager.UI_comps.UserGuide;
+import brewcrew.com.taskmanager.UIComponents.UserGuide;
 import brewcrew.com.taskmanager.helperClasses.MyTasks;
-
 public class Editor extends AppCompatActivity {
 
     private static final String TAG = "Editor";
-
     public static TextView date, timeView;
-
     public static EditText desc, titl;
-
     public static ImageView notifiy;
-
     private MyTasks tasks;
-
     static boolean first_run = true;
-
-    @Override
+    private database db;
+     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_editor);
         getSupportActionBar().setTitle(null);
+
         date = (TextView) findViewById(R.id.date);
         titl = (EditText) findViewById(R.id.titl);
         desc = (EditText) findViewById(R.id.desc);
         notifiy = (ImageView) findViewById(R.id.notify);
-        Log.i(TAG, "onCreate: " + first_run);
-//        if (first_run) {
-
-        showDialog();
-//            first_run = false;
-        Log.i(TAG, "onCreate:after loop " + first_run);
-//        }
-
         timeView = (TextView) findViewById(R.id.time_view);
 
+//
+        Log.i(TAG, "onCreate: " + first_run);
+//        if (first_run) {
+        showDialog();
+//            first_run = false;blue
+        Log.i(TAG, "onCreate:after loop " + first_run);
+//        }
         if (getIntent().hasExtra("from_onCleck")) {
+
             tasks = (MyTasks) MainActivity.li.get(getIntent().getIntExtra("from_onCleck", 0));
             date.setText(tasks.getDate());
             titl.setText(tasks.getTitle());
             desc.setText(tasks.getDesc());
             timeView.setText(tasks.getTime());
 
+
         }
 
     }
-
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
-
         if (keyCode == KeyEvent.KEYCODE_BACK)
             if (tasks != null) {
-                if (!tasks.getDate().toString().equals(date.getText().toString()) | !tasks.getDesc().toString().equals(desc.getText().toString()) | !
-                        tasks.getTitle().equals(titl.getText().toString().toString())
-                        | !tasks.getTime().toString().equals(timeView.getText().toString())) {
+                db=new database(this);
+                if (!tasks.getDate().equals(date.getText().toString())
+                        | !tasks.getDesc().equals(desc.getText().toString())
+                        | !tasks.getTitle().equals(titl.getText().toString())
+                        | !tasks.getTime().equals(timeView.getText().toString())|tasks.isNotifyUser()!=notifyUser(notifiy.getDrawable()) ) {
+                    ContentValues values=new ContentValues();
+                    values.put(databaseEntries.description,desc.getText().toString());
+                    values.put(databaseEntries.date,date.getText().toString());
+                    values.put(databaseEntries.time,timeView.getText().toString());
+                    values.put(databaseEntries.title,titl.getText().toString());
+                    values.put(databaseEntries.notifyUser,notifyUser(notifiy.getDrawable()));
+                    String[] args={String.valueOf(getIntent().getIntExtra("from_onCleck",0)) };
+                    db.getReadableDatabase().update(databaseEntries.tableName,values,"ID",args);
 
-                    tasks.setDesc(desc.getText().toString());
-                    tasks.setDate(Date_formatter(date.getText()));
-                    tasks.setTime(timeView.getText().toString());
-                    tasks.setTitle(titl.getText().toString());
 
+
+// tasks.setDesc(desc.getText().toString());//    DialogFragment  dialogFragment=new DialogFragment();
+//    dialogFragment.show(getSupportFragmentManager(),"datePicker.class");
+//                    tasks.setDate(Date_formatter(date.getText()));
+//                    tasks.setTime(timeView.getText().toString());
+//                    tasks.setTitle(titl.getText().toString());
                     super.onKeyDown(keyCode, event);
                 }
 
             } else if (desc.getText().toString().length() != 0) {
-
-                if (date.getText().toString().length() != 0) {
+                if (date.getText().toString().length() != 0&notifyUser(notifiy.getDrawable())) {
                     String tym = null;
                     if (timeView.getText().toString().length() != 0) {
                         tym = timeView.getText().toString();
                     } else
                         tym = "00:00";
-                    MyTasks m = new MyTasks(date.getText().toString(), titl.getText().toString(),
-                            desc.getText().toString(),
-                            tym
-                            , false);
-
+                    MyTasks m = new MyTasks(desc.getText().toString(),titl.getText().toString(),  tym,date.getText().toString(),false,notifyUser(notifiy.getDrawable()));
                     MainActivity.li.add(m);
                 } else {
-
+                    notifiy.setImageDrawable(getDrawable(R.drawable.noti_on));
                     AlertDialog.Builder ad = new AlertDialog.Builder(this, R.style.ThemeOverlay_AppCompat_Dialog);
-                    ad.setMessage("Please select date for the task");
+                    ad.setMessage("Please select date or toggle \n the notification icon");
                     ad.setPositiveButton("Set date", new DialogInterface.OnClickListener() {
-
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
-
                             showDatePicker();
                         }
                     });
-
                     ad.setCancelable(true);
 
                     AlertDialog adg = ad.create();
@@ -121,72 +117,53 @@ public class Editor extends AppCompatActivity {
 
                 }
             }
-
         return super.onKeyDown(keyCode, event);
     }
 
-    public void showDatePicker(View v) {
 
+    /*
+    * Helper methods
+    *
+    * */
+    public void showDatePicker(View v) {
         datePicker pic = new datePicker();
         pic.show(getSupportFragmentManager(), "datepicker");
 
-//    DialogFragment  dialogFragment=new DialogFragment();
-//    dialogFragment.show(getSupportFragmentManager(),"datePicker.class");
-
     }
+
 
     public void showDatePicker() {
-
-
         datePicker pic = new datePicker();
         pic.show(getSupportFragmentManager(), "datepicker");
-
-//    DialogFragment  dialogFragment=new DialogFragment();
-//    dialogFragment.show(getSupportFragmentManager(),"datePicker.class");
-
     }
-
     public void showTimePicker(View v) {
-
         new timePicker().show(getFragmentManager(), "time_picker");
 
     }
-
     public void showDialog() {
-
         FragmentManager fm = getFragmentManager();
-        UserGuide ug = new UserGuide();
-//    FloatingActionButton fab=ug.getView().findViewById(R.id.fab2);
-//    fab.setOnClickListener(new View.OnClickListener() {
-//        @Override
-//        public void onClick(View view) {
-//         ug.dismiss();
-//        }
-//    });
-        ug.show(fm, "dialog");
+        UserGuide userGuide = new UserGuide();
+        userGuide.show(fm, "dialog");
     }
-
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-
         super.onSaveInstanceState(outState);
     }
-
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-
         return super.onTouchEvent(event);
     }
-
     static String Date_formatter(CharSequence charSequence) {
-
         String temp = charSequence.subSequence(charSequence.length() - 1 - 4, charSequence.length() - 1).toString();
         if (temp != String.valueOf(Calendar.getInstance().YEAR))
             return charSequence.toString();
         else
-
             return charSequence.toString().replace(temp, "");
 
     }
+
+private boolean notifyUser(Drawable drawable){
+return  drawable==getDrawable(R.drawable.noti_on)? true:false;
+}
 
 }
--- app/src/main/java/brewcrew/com/taskmanager/activities/MainActivity.java
+++ app/src/main/java/brewcrew/com/taskmanager/activities/MainActivity.java
@@ -1,11 +1,9 @@
 package brewcrew.com.taskmanager.activities;
-
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.BoolRes;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
@@ -20,78 +18,60 @@ import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-import brewcrew.com.taskmanager.Db.database;
-import brewcrew.com.taskmanager.Db.databaseEntries;
-import brewcrew.com.taskmanager.Pickers.datePicker;
+import brewcrew.com.taskmanager.db.database;
+import brewcrew.com.taskmanager.db.databaseEntries;
+import brewcrew.com.taskmanager.pickers.datePicker;
 import brewcrew.com.taskmanager.R;
 import brewcrew.com.taskmanager.helperClasses.MyTasks;
 import brewcrew.com.taskmanager.helperClasses.taskRecycler;
-
 public class MainActivity extends AppCompatActivity implements taskRecycler.touchListener {
 
     private static final String TAG = "MainActivity";
-
-    static ArrayList<MyTasks> li=new ArrayList<MyTasks>();
-
+    static ArrayList<MyTasks> li ;
     taskRecycler taskRec;
 
-    private Cursor cursor;
-
     LinearLayoutManager linearLayoutManager;
-
     GridLayoutManager gridLayoutManager;
-
     RecyclerView recycler;
-
     Calendar calendar = Calendar.getInstance();
-
     TextView notice;
 
-    private database db;
-
     private Intent add_edit;
-
     private int date_int = calendar.get(Calendar.DATE);
-
     String date_tommo = date_int + "/"
             + datePicker.give_month_in_string(calendar.get(Calendar.MONTH))
             + "/" + calendar.get(Calendar.YEAR);
-
     private SharedPreferences sharedPreferences;
-
     boolean state;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         notice = (TextView) findViewById(R.id.notice_view);
         recycler = (RecyclerView) findViewById(R.id.recycler);
+        Cursor cursor;
+        cursor = database.getCursor(databaseEntries.selectAllQuery, this);
+        Async async=new Async();
+        async.execute(cursor);
 
-//        try {
-//            taskRec = new taskRecycler(new Async().get(), this);
-//        } catch (InterruptedException IE) {
-//
-//            Log.e(TAG, "onCreate:" + IE.getMessage());
-//        } catch (ExecutionException EE) {
-//
-//            Log.e(TAG, "onCreate:" + EE.getMessage());
-//        }
+        try {
+            taskRec = new taskRecycler(async.get(), this);
+        } catch (InterruptedException IE) {
+
+            Log.e(TAG, "onCreate:" + IE.getMessage());
+        } catch (ExecutionException EE) {
+
+            Log.e(TAG, "onCreate:" + EE.getMessage());
+        }
 
         visibilitySetter();
-        db = new database(this);
-        cursor = database.getCursor(databaseEntries.selectAllQuery, this);
-
-        Log.i(TAG, "onCreate:" +cursor.getColumnName(5));
 
 
         linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         gridLayoutManager = new GridLayoutManager(this, 2, LinearLayoutManager.VERTICAL, false);
-        recycler.setLayoutManager(gridLayoutManager);
+        recycler.setLayoutManager(linearLayoutManager);
         recycler.setAdapter(taskRec);
 
 
@@ -102,49 +82,36 @@ public class MainActivity extends AppCompatActivity implements taskRecycler.touc
         * */
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
-
             @Override
             public void onClick(View view) {
-
                 add_edit = new Intent(getApplicationContext(), Editor.class);
-
                 add_edit.putExtra("default date", date_tommo);
-
                 startActivity(add_edit);
 
             }
         });
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
-
         super.onCreateContextMenu(menu, v, menuInfo);
         // getMenuInflater().inflate();
-
     }
-
     @Override
     public boolean onContextItemSelected(MenuItem item) {
-
         return super.onContextItemSelected(item);
     }
-
     @Override
-
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
         //noinspection SimplifiableIfStatement
         switch (id) {
             case R.id.toggle_layout:
@@ -152,22 +119,17 @@ public class MainActivity extends AppCompatActivity implements taskRecycler.touc
                 break;
 
         }
-
         return super.onOptionsItemSelected(item);
     }
-
     @Override
     protected void onRestart() {
-
         super.onRestart();
         visibilitySetter();
         recycler.setAdapter(taskRec);
 
     }
-
     @Override
     protected void onResume() {
-
         super.onResume();
         visibilitySetter();
         recycler.setAdapter(taskRec);
@@ -177,23 +139,19 @@ public class MainActivity extends AppCompatActivity implements taskRecycler.touc
    /*
   Helper methods
     */
-
     // item click events
     @Override
     public void onCleck(int clickedIndex) {
-
         add_edit = new Intent(getApplicationContext(), Editor.class);
-        add_edit.putExtra("from_onCleck", clickedIndex);
+        add_edit.putExtra("from_onClick", clickedIndex);
         startActivity(add_edit);
         Log.i(TAG, "onCleck clicked:" + clickedIndex);
     }
-
     /*
     * Show banner if no event is present in list
     *
     * */
     void visibilitySetter() {
-
         if (li.size() < 1) {
             notice.setVisibility(View.VISIBLE);
             recycler.setVisibility(View.INVISIBLE);
@@ -203,18 +161,13 @@ public class MainActivity extends AppCompatActivity implements taskRecycler.touc
 
         }
     }
-
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-
         super.onSaveInstanceState(outState);
     }
-
     void toggle(MenuItem item) {
-
         if (state) {
             Log.i(TAG, "toggle:" + state);
-
             recycler.setLayoutManager(gridLayoutManager);
             item.setIcon(getDrawable(R.drawable.ic_menu));
             sharedPreferences.edit().putBoolean("LinearLayoutActive", false).apply();
@@ -224,49 +177,42 @@ public class MainActivity extends AppCompatActivity implements taskRecycler.touc
         recycler.setLayoutManager(linearLayoutManager);
         sharedPreferences.edit().putBoolean("LinearLayoutActive", true).apply();
     }
-
-    private static Boolean truefalser(int status) {
+    private static Boolean trueFalser(int status) {
         return (status == 0) ? false : true;
     }
+
+
+
     /*
     *Asynchronous Handling of data
     * */
-
-    class Async extends AsyncTask<Cursor, Void, ArrayList> {
+   private class Async extends AsyncTask<Cursor, Void, ArrayList> {
 
         @Override
         protected void onPreExecute() {
-//            li = new ArrayList<>();
+            li = new ArrayList<>();
             super.onPreExecute();
 
         }
-
         @Override
         protected ArrayList doInBackground(Cursor... cursores) {
             Cursor curs = cursores[0];
-
             int descColumn = curs.getColumnIndexOrThrow(databaseEntries.description);
             int titleColumn = curs.getColumnIndexOrThrow(databaseEntries.title);
             int timeColumn = curs.getColumnIndexOrThrow(databaseEntries.time);
             int dateColumn = curs.getColumnIndexOrThrow(databaseEntries.date);
             int statusColumn = curs.getColumnIndexOrThrow(databaseEntries.status);
-
+            int notifyColumn=curs.getColumnIndex(databaseEntries.notifyUser);
             while (curs.moveToNext()) {
-//                li.add(new MyTasks(curs.getString(descColumn),
-//                        curs.getString(titleColumn),
-//                        curs.getString(timeColumn),
-//                        curs.getString(dateColumn),
-//                        truefalser(curs.getInt(statusColumn))));
-                Log.i(TAG,curs.getString(titleColumn)+" "+" "+
-                        curs.getString(timeColumn)+" "+
-                   curs.getString(dateColumn) );
-
+                li.add(new MyTasks(curs.getString(descColumn),
+                        curs.getString(titleColumn),
+                        curs.getString(timeColumn),
+                        curs.getString(dateColumn),
+                        trueFalser(curs.getInt(statusColumn)),trueFalser(curs.getInt(notifyColumn))));
             }
-
             curs.close();
             return li;
         }
-
         @Override
         protected void onPostExecute(ArrayList list) {
             super.onPostExecute(list);
